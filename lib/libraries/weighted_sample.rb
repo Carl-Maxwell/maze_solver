181b26278a6104781be73c3dec651523d43d50b8
Carl Maxwell <carl.maxwell@gmail.com>
2015-08-30 17:40:09 -0700

    fixing weighted sample
    previously if called on like ["a", "b", "c"]
    
    it would map indexes to like
    
    0   1  2   3  4  5
    -3 -2 -1   0  1  2
    
    so that -3 would become the index 3
    
    so the return values of weighted sample were like:
    
    ["a", "b", "c"] => one of ["a", "b", "c", nil]
    
    though the nil rarely showed up (being at one end of a bell curve)

--- lib/libraries/weighted_sample.rb
+++ lib/libraries/weighted_sample.rb
@@ -1,6 +1,21 @@
 class Array
   def weighted_sample
     l = self.length * 2
-    self[ (((rand(l) + rand(l) + rand(l) + rand(l)))/4 - self.length).abs ]
+
+    divisor = 4
+
+    thing = 0
+
+    divisor.times { thing += rand(l) }
+
+    thing /= divisor
+
+    thing -= self.length
+
+    if thing < 0
+      thing = thing.abs() - 1
+    end
+
+    self[ thing ]
   end
 end
